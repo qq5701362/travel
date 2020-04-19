21068c4c75359d31393453414903559fce4e329e
“王贵康 <15777239195@163.com>
2020-04-19 20:35:45 +0800

    修复广告修改不成功的问题。

--- src/jx/tour/controller/BackAdsController.java
+++ src/jx/tour/controller/BackAdsController.java
@@ -21,75 +21,75 @@ import jx.tour.utils.PageUtils;
 @Controller
 @RequestMapping("/backAds")
 public class BackAdsController { 
-	@Autowired
-	private BackAdsService backAdsService;
-	@Autowired
-	private UploadFileService uploadFileService;
-	@Autowired
-	private PageUtils pageUtils;
+    @Autowired
+    private BackAdsService backAdsService;
+    @Autowired
+    private UploadFileService uploadFileService;
+    @Autowired
+    private PageUtils pageUtils;
     //得到所有的广告信息进行分页
-	@RequestMapping("/requestAllAds")
-	public String requestAllAds(@RequestParam(required = false,defaultValue = "1",value = "page")Integer page,Model model) {  
-	    //引入分页查询，使用PageHelper分页功能  
-	    //在查询之前传入当前页，然后多少记录   
-	    PageHelper.startPage(page,5);      
-	    //startPage后紧跟的这个查询就是分页查询  
-	    List<Ads> list = backAdsService.getAllAds();
+    @RequestMapping("/requestAllAds")
+    public String requestAllAds(@RequestParam(required = false,defaultValue = "1",value = "page")Integer page,Model model) {  
+        //引入分页查询，使用PageHelper分页功能  
+        //在查询之前传入当前页，然后多少记录   
+        PageHelper.startPage(page,5);      
+        //startPage后紧跟的这个查询就是分页查询  
+        List<Ads> list = backAdsService.getAllAds();
 
-	    //使用PageInfo包装查询结果，只需要将pageInfo交给页面就可以  
-	    PageInfo<Ads> pageInfo = new PageInfo<>(list,5);  
-	    pageUtils.setCurrentPageNum(page);
-	    //pageINfo封装了分页的详细信息，也可以指定连续显示的页数    
-	    model.addAttribute("pageInfo",pageInfo);  
-	    return "admin/ads";     
-	}
+        //使用PageInfo包装查询结果，只需要将pageInfo交给页面就可以  
+        PageInfo<Ads> pageInfo = new PageInfo<>(list,5);  
+        pageUtils.setCurrentPageNum(page);
+        //pageINfo封装了分页的详细信息，也可以指定连续显示的页数    
+        model.addAttribute("pageInfo",pageInfo);  
+        return "admin/ads";     
+    }
 
-	// 编辑特产信息
-	@RequestMapping("/editAds")
-	public String editSpecialty(Model model,MultipartFile file, Ads ads) throws Exception {
-		String save = uploadFileService.uploadAdsImag(file);
-		ads.setPic(save);
-		ads.setStage(1);
-		ads.setPtime(new Date());
-		try{
-		   backAdsService.editAds(ads);
-		   model.addAttribute("error","修改错误,可能未上传图片");
-			return "admin/error";
-		}catch(Exception e){
-			
-		}
-		return "redirect:/backAds/requestAllAds";  
-	}
-	
-	@RequestMapping("/upAds")
-	public String upAds(Model model,int id){
-		try{  
-			backAdsService.upAds(id);
-		}catch(Exception e){
-			model.addAttribute("error","上架信息有误");
-			return "admin/error";
-		}
-		
-		return "redirect:/backAds/requestAllAds";
-	}
-	@RequestMapping("/downAds")
-	public String downAds(Model model,int id){  
-		backAdsService.downAds(id);
-		return "redirect:/backAds/requestAllAds";
-	}
-	@RequestMapping("/addAds")
-	public String addAds(MultipartFile file,Ads ads,Model model) throws Exception{
-		String save = uploadFileService.uploadAdsImag(file);
-		ads.setPic(save);
-		ads.setStage(1);
-		ads.setPtime(new Date()); 
-		try {
-			backAdsService.addAds(ads);
-		} catch (Exception e) {
-			model.addAttribute("error","还有信息未填写");
-			return "admin/error";
-		}
-		
-		return "redirect:/backAds/requestAllAds";
-	}
+    // 编辑特产信息
+    @RequestMapping("/editAds")
+    public String editSpecialty(Model model,MultipartFile file, Ads ads) throws Exception {
+        String save = uploadFileService.uploadAdsImag(file);
+        ads.setPic(save);
+        ads.setStage(1);
+        ads.setPtime(new Date());
+        try{
+           backAdsService.editAds(ads);
+           
+        }catch(Exception e){
+            model.addAttribute("error","修改错误,可能未上传图片");
+            return "admin/error";
+        }
+        return "redirect:/backAds/requestAllAds";  
+    }
+    
+    @RequestMapping("/upAds")
+    public String upAds(Model model,int id){
+        try{  
+            backAdsService.upAds(id);
+        }catch(Exception e){
+            model.addAttribute("error","上架信息有误");
+            return "admin/error";
+        }
+        
+        return "redirect:/backAds/requestAllAds";
+    }
+    @RequestMapping("/downAds")
+    public String downAds(Model model,int id){  
+        backAdsService.downAds(id);
+        return "redirect:/backAds/requestAllAds";
+    }
+    @RequestMapping("/addAds")
+    public String addAds(MultipartFile file,Ads ads,Model model) throws Exception{
+        String save = uploadFileService.uploadAdsImag(file);
+        ads.setPic(save);
+        ads.setStage(1);
+        ads.setPtime(new Date()); 
+        try {
+            backAdsService.addAds(ads);
+        } catch (Exception e) {
+            model.addAttribute("error","还有信息未填写");
+            return "admin/error";
+        }
+        
+        return "redirect:/backAds/requestAllAds";
+    }
 }
--- src/jx/tour/service/impl/UploadFileServiceImp.java
+++ src/jx/tour/service/impl/UploadFileServiceImp.java
@@ -11,20 +11,20 @@ import jx.tour.service.UploadFileService;
 
 @Service
 public class UploadFileServiceImp implements UploadFileService {
-	//E:\tts9\apache-tomcat-8.0.42\wtpwebapps\travel\pic
-	//D:\\00x_IT\\new2\\workspace\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\travel\\pic
-	//E:\tts9\apache-tomcat-8.0.42\wtpwebapps\travel\pic
-	private final String origin = "D:\\00x_IT\\new2\\workspace\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\travel\\pic"; 
-	
-	/**
-	 * 我的游记，添加多张图片
-	 */
-	@Override
-	public List<String> uploadFile(MultipartFile[] files) throws Exception{
-		List<String> picPath = null;
+    //E:\tts9\apache-tomcat-8.0.42\wtpwebapps\travel\pic
+    //D:\\00x_IT\\new2\\workspace\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\travel\\pic
+    //E:\tts9\apache-tomcat-8.0.42\wtpwebapps\travel\pic
+    private final String origin = "D:\\00x_IT\\new2\\workspace\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\travel\\pic"; 
+    
+    /**
+     * 我的游记，添加多张图片
+     */
+    @Override
+    public List<String> uploadFile(MultipartFile[] files) throws Exception{
+        List<String> picPath = null;
         // 先判断文件files不为空
         if (files != null && files.length > 0) {  
-        	 picPath = new ArrayList<String>();
+             picPath = new ArrayList<String>();
             for (MultipartFile file : files) { //循环遍历，取出单个文件，下面的操作和单个文件就一样了
                 if (!file.isEmpty()) {//这个判断必须加上
                     // 获得原始文件名
@@ -44,13 +44,13 @@ public class UploadFileServiceImp implements UploadFileService {
                 }
             }
         }
-		return picPath;
-	}
+        return picPath;
+    }
 
-	@Override
-	public String uploadUserImag(MultipartFile file) throws Exception {
-		String save = "";
-		 if (!file.isEmpty()) {//这个判断必须加上
+    @Override
+    public String uploadUserImag(MultipartFile file) throws Exception {
+        String save = "";
+         if (!file.isEmpty()) {//这个判断必须加上
              // 获得原始文件名
              String originalFileName = file.getOriginalFilename();
              //新文件名
@@ -66,14 +66,14 @@ public class UploadFileServiceImp implements UploadFileService {
              
 
          }
-		 return save;
-	}
-	@Override
-	 public List<String>  uploadScenicImag(MultipartFile[] files,int scenicNum)throws Exception {
-		List<String> picPath = null;
+         return save;
+    }
+    @Override
+     public List<String>  uploadScenicImag(MultipartFile[] files,int scenicNum)throws Exception {
+        List<String> picPath = null;
         // 先判断文件files不为空
         if (files != null && files.length > 0) {
-        	 picPath = new ArrayList<String>();
+             picPath = new ArrayList<String>();
             for (MultipartFile file : files) { //循环遍历，取出单个文件，下面的操作和单个文件就一样了
                 if (!file.isEmpty()) {//这个判断必须加上  
                     // 获得原始文件名
@@ -94,14 +94,14 @@ public class UploadFileServiceImp implements UploadFileService {
             }
         }
      return picPath;
-	}
+    }
 
-	@Override
-	public List<String> uploadSpecialtyImag(MultipartFile[] files, int specialtyNum) throws Exception {
-		List<String> picPath = null;
+    @Override
+    public List<String> uploadSpecialtyImag(MultipartFile[] files, int specialtyNum) throws Exception {
+        List<String> picPath = null;
         // 先判断文件files不为空
         if (files != null && files.length > 0) {
-        	 picPath = new ArrayList<String>();
+             picPath = new ArrayList<String>();
             for (MultipartFile file : files) { //循环遍历，取出单个文件，下面的操作和单个文件就一样了
                 if (!file.isEmpty()) {//这个判断必须加上  
                     // 获得原始文件名
@@ -122,15 +122,15 @@ public class UploadFileServiceImp implements UploadFileService {
             }
         }
      return picPath;
-	}
+    }
 
-	@Override
-	public List<String> uploadHotelImag(MultipartFile[] files, int hNum) throws Exception {
-		 
-		List<String> picPath = null;
+    @Override
+    public List<String> uploadHotelImag(MultipartFile[] files, int hNum) throws Exception {
+         
+        List<String> picPath = null;
         // 先判断文件files不为空
         if (files != null && files.length > 0) {
-        	 picPath = new ArrayList<String>();
+             picPath = new ArrayList<String>();
             for (MultipartFile file : files) { //循环遍历，取出单个文件，下面的操作和单个文件就一样了
                 if (!file.isEmpty()) {//这个判断必须加上  
                     // 获得原始文件名
@@ -151,11 +151,11 @@ public class UploadFileServiceImp implements UploadFileService {
             }
         }
      return picPath;
-	}
+    }
 
-	@Override
-	public String uploadAdsImag(MultipartFile file) throws Exception {
-		  // 获得原始文件名
+    @Override
+    public String uploadAdsImag(MultipartFile file) throws Exception {
+          // 获得原始文件名
         String originalFileName = file.getOriginalFilename();
         //新文件名
         String newFileName = UUID.randomUUID() + originalFileName.substring(originalFileName.lastIndexOf("."));
@@ -167,12 +167,12 @@ public class UploadFileServiceImp implements UploadFileService {
         File newFile = new File(pathRoot);
         //将内存的图片数据写入磁盘
         file.transferTo(newFile);  
-		return save;
-	}
+        return save;
+    }
 
-	@Override
-	public String uploadInformationImag(MultipartFile file) throws Exception {
-		  // 获得原始文件名
+    @Override
+    public String uploadInformationImag(MultipartFile file) throws Exception {
+          // 获得原始文件名
         String originalFileName = file.getOriginalFilename();
         //新文件名
         String newFileName = UUID.randomUUID() + originalFileName.substring(originalFileName.lastIndexOf("."));
@@ -184,23 +184,23 @@ public class UploadFileServiceImp implements UploadFileService {
         File newFile = new File(pathRoot);
         //将内存的图片数据写入磁盘
         file.transferTo(newFile);  
-		return save;
-	}
+        return save;
+    }
 
-	@Override
-	public String uploadContentImag(MultipartFile file,Content content) throws Exception {
-		int areanum = content.getAreanum();
-		int categoryid = content.getCategoryid();
-		String save = "";
-		 // 获得原始文件名
+    @Override
+    public String uploadContentImag(MultipartFile file,Content content) throws Exception {
+        int areanum = content.getAreanum();
+        int categoryid = content.getCategoryid();
+        String save = "";
+         // 获得原始文件名
         String originalFileName = file.getOriginalFilename();
         //新文件名
         String newFileName = UUID.randomUUID() + originalFileName.substring(originalFileName.lastIndexOf("."));
         if(categoryid==0){
-        	save = "/area/scenic/"+newFileName;
+            save = "/area/scenic/"+newFileName;
         }
         else{
-        	save = "/area/specialty/"+newFileName;
+            save = "/area/specialty/"+newFileName;
         }
         //获得物理路径,即新文件将要保存的路径
         //String pathRoot = "E:/upload/temp/"+save;
@@ -209,6 +209,6 @@ public class UploadFileServiceImp implements UploadFileService {
         File newFile = new File(pathRoot);
         //将内存的图片数据写入磁盘
         file.transferTo(newFile);  
-		return save;
-	}
+        return save;
+    }
 }
